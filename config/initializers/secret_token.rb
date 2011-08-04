e9c6d5cdb01b9f19360300773823585a2a380ad5
Paweł Sobolewski <pawel.sobolewski@amberbit.com>
2011-08-04 10:51:41 +0200

    Created project checkn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkn::Application.config.secret_token = 'bde692b14a5e1a9c40e31ea89b46d92a5291a8443f13a612623956fd54f97f4d6c55865ca853f477d93f13e29b92eba1a1e848b64355eb89a4094fdd9bf74736'
