124b59ce3ce3fde7a5d3e13203dfcc55e0cefc57
Paweł Sobolewski <pawel.sobolewski@amberbit.com>
2011-08-18 17:05:00 +0200

    Submenu lines animation

--- public/javascripts/app/layouts/header.js
+++ public/javascripts/app/layouts/header.js
@@ -5,9 +5,48 @@ $(function(){
     $(this).parent().toggleClass("hovered");
   });
 
-  $("#subheader p a").mouseover(function(e){
-    $(this).parent().toggleClass("hovered");
-  }).mouseout(function(){
-    $(this).parent().toggleClass("hovered");
+  $("#subheader p a.left").hover(function() {
+    if (!$(this).parent().hasClass("active")) {
+      $(this).parent().toggleClass("hovered");
+      $(".active-line").toggle();
+      $("#left-line").stop(false, true).fadeIn();
+    }
+  }, function() {
+    if (!$(this).parent().hasClass("active")) {
+      $(this).parent().toggleClass("hovered");
+      $("#left-line").stop(false, true).fadeOut('fast', function() {
+        $(".active-line").toggle();
+      });
+    }
+  });
+
+  $("#subheader p a.middle").hover(function(e){
+     if (!$(this).parent().hasClass("active")) {
+      $(this).parent().toggleClass("hovered");
+      $(".active-line").toggle();
+      $("#middle-line").stop(false, true).fadeIn();
+    }
+  }, function() {
+    if (!$(this).parent().hasClass("active")) {
+      $(this).parent().toggleClass("hovered");
+      $("#middle-line").stop(false, true).fadeOut('fast', function() {
+        $(".active-line").toggle();
+      });
+    }
+  });
+
+  $("#subheader p a.right").hover(function(e){
+    if (!$(this).parent().hasClass("active")) {
+      $(this).parent().toggleClass("hovered");
+      $(".active-line").toggle();
+      $("#right-line").stop(false, true).fadeIn();
+    }
+  }, function() {
+    if (!$(this).parent().hasClass("active")) {
+      $(this).parent().toggleClass("hovered");
+      $("#right-line").stop(false, true).fadeOut('fast', function() {
+        $(".active-line").toggle();
+      });
+    }
   });
 });
