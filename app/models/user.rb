2e652c7f3ef30f9500187dbddd9b8e45bfea157c
Paweł Sobolewski <pawel.sobolewski@amberbit.com>
2012-01-07 16:13:34 +0100

    Remove sign up for admins :]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me
