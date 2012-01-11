23a1e613909fb4829c8f0f798c26c2afa67092e2
Paweł Sobolewski <pawel.sobolewski@amberbit.com>
2012-01-11 09:57:43 +0100

    Fix for ie - news + admin panel bg

--- public/javascripts/app/admin.js
+++ public/javascripts/app/admin.js
@@ -22,4 +22,8 @@ $(function(){
   }).mouseout(function(){
     $(this).parent().toggleClass("hovered");
   });
+  
+  if ($("html").hasClass("ie6") || $("html").hasClass("ie7") || $("html").hasClass("ie8")) {
+    $("html").prepend('<div class="ie_bg_fix"></div>');
+  }
 });
