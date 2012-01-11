367dfb7e9836d61d12dc1692a82889747ef1e85a
Paweł Sobolewski <pawel.sobolewski@amberbit.com>
2012-01-11 09:14:27 +0100

    Background fix for ie

--- public/javascripts/app/layouts/application.js
+++ public/javascripts/app/layouts/application.js
@@ -4,4 +4,10 @@ $(function(){
   }).mouseout(function(){
     $(this).parent().toggleClass("hovered");
   });
+  
+  if ($("html").hasClass("ie6") || $("html").hasClass("ie7") || $("html").hasClass("ie8")) {
+    $("html").prepend('<div class="ie_bg_fix"></div>');
+  } else {
+    $("#main .static").addClass("normal");
+  }
 });
